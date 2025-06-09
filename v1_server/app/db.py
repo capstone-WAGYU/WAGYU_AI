4883c682eb49211693b22b81145257cda15bd87b
yeeeengyu <153285299+yeeeeeeeeeeengyu@users.noreply.github.com>
2025-06-10 00:32:36 +0900

    크롤링 추가, 가입 조건 및 기간 추가

--- v1_server/app/db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/")
-db = client["bank"]
-invrecom_collection = db["invrecom_print"]
-
-def save_result_to_db(result):
-    invrecom_collection.insert_one({"result": result})
